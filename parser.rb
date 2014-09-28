7957d7caddd1a8df37eadce7f40bf7d4d83a4cc2
Marta Paciorkowska <marta@paciorkowska.pl>
2014-09-28 21:00:58 +0200

    DRY is dead

--- parser.rb
+++ parser.rb
@@ -1,7 +1,10 @@
 require 'nokogiri'
+require 'mechanize'
+require 'open-uri'
 
 class Parser
   include Nokogiri
+
   # list heavily influenced by
   # http://www.stackdriver.com/top-devops-influencers-blogs-follow/
   BLOG_LIST = { 'Andrew Hay' => 'http://www.andrewhay.ca/',
@@ -9,7 +12,6 @@ class Parser
                 'Gene Kim' => 'http://itrevolution.com/devops-blog/',
                 'ScriptRock' => 'http://scriptrock.com/blog/',
                 'DevOpsGuys' => 'http://blog.devopsguys.com/',
-                'Adrian Cockcroft' => 'http://perfcap.blogspot.com/',
                 'Kitchen Soap' => 'http://www.kitchensoap.com/',
                 'Scalable Startups' => 'http://www.iheavy.com/blog/',
                 'Socialized Software' => 'http://socializedsoftware.com/',
@@ -18,6 +20,7 @@ class Parser
                 'Kevin Behr' => 'http://www.kevinbehr.com/',
                 'Build Doctor' => 'http://build-doctor.com/',
                 'TechnoCalifornia' => 'http://technocalifornia.blogspot.com/',
+                'Adrian Cockcroft' => 'http://perfcap.blogspot.com/',
                 'Test Obsessed' => 'http://testobsessed.com/',
                 'Dominica DeGrandis' => 'http://www.ddegrandis.com/blog',
                 'Snipe.net' => 'http://www.snipe.net/',
@@ -26,7 +29,38 @@ class Parser
                 'Patrick Debois' => 'http://www.jedi.be/',
                 'Chris Read' => 'http://blog.chris-read.net/',
                 'Kartar' => 'http://www.kartar.net/',
-                'Morethanseven' => 'http://www.morethanseven.net/',
-                'blog dot lusis' => 'http://blog.lusis.org/'
+                'Morethanseven' => 'http://www.morethanseven.net/',            #+
+                'blog dot lusis' => 'http://blog.lusis.org/blog/archives'      #+
               }
+
+  class << self
+    def create_files
+      BLOG_LIST.each_key do |file_name|
+        safe_name = file_name.gsub(' ', '_').gsub('.', '_').downcase
+        instance_variable_set("@#{safe_name}_database", (File.new("#{Dir.pwd}/corpuses/#{safe_name}.txt", 'w') unless File.exist?("#{Dir.pwd}/corpuses/#{safe_name}.txt")))
+      end
+    end
+  end
+
+  Parser.create_files
+  fetcher = Mechanize.new
+
+  blog_dot_lusis_main = fetcher.get(BLOG_LIST['blog dot lusis'])
+  fetcher.page.links_with(href: /.*\/20.*/).each do |link|
+    p link
+    link.click
+    fetcher.page.search('//*[@id="content"]/div/article/div/p').each do |paragraph|
+      @blog_dot_lusis_database << paragraph.content
+    end
+  end
+
+  morethanseven_main = fetcher.get(BLOG_LIST['Morethanseven'])
+  fetcher.page.links_with(href: /.*\/20.*/).each do |link|
+    p link
+    link.click
+    fetcher.page.search('//*[@id="content"]/div/article/div/p').each do |paragraph|
+      @morethanseven_database << paragraph.content
+    end
+  end
+
 end
